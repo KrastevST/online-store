7fd31ba0b9d9687a4507c00f53f3684d4e2c61dc
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-03-23 04:09:35 +0200

    added writeAll and randomId

--- online-store/repositories/users.js
+++ online-store/repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepository {
   constructor(filename) {
@@ -21,10 +22,29 @@ class UsersRepository {
       })
     );
   }
+
+  async create(attrs) {
+    attrs.id = this.randomId();
+    
+    const records = await this.getAll();
+    records.push(attrs);
+
+    await this.writeAll(records);
+  }
+
+  async writeAll(records) {
+    await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString('hex');
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
+
+  await repo.create({ email: 'test@test.com', password: 'password' });
 
   const users = await repo.getAll();
 
