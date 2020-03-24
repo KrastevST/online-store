39964c90ef75ab360ef418ad3839a49cc1906bbb
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-03-24 07:53:31 +0200

    instance of UsersRepository rxported

--- online-store/repositories/users.js
+++ online-store/repositories/users.js
@@ -82,11 +82,4 @@ class UsersRepository {
   }
 }
 
-const test = async () => {
-  const repo = new UsersRepository('users.json');
-
-  const user = await repo.getOneBy({ password: 'mypassword', email: 'test@test.com' });
-  console.log(user);
-};
-
-test();
+module.exports = new UsersRepository('users.json');
