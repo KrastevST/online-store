a2a08536426c1df015975c93401458f80e6e632d
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-04-01 19:47:39 +0300

    added styling

--- online-store/routes/admin/auth.js
+++ online-store/routes/admin/auth.js
@@ -4,7 +4,13 @@ const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
-const { requireEmail, requirePassword, requirePasswordConfirmation } = require('./validators');
+const {
+  requireEmail,
+  requirePassword,
+  requirePasswordConfirmation,
+  requireEmailExists,
+  requireValidPasswordForUser
+} = require('./validators');
 
 const router = express.Router();
 
@@ -36,29 +42,26 @@ router.get('/signout', (req, res) => {
 });
 
 router.get('/signin', (req, res) => {
-  res.send(signinTemplate());
+  res.send(signinTemplate({}));
 });
 
-router.post('/signin', async (req, res) => {
-  const { email, password } = req.body;
+router.post(
+  '/signin',
+  [requireEmailExists, requireValidPasswordForUser],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-  const user = await usersRepo.getOneBy({ email: email });
+    if (!errors.isEmpty()) {
+      return res.send(signinTemplate({ errors }));
+    }
+    
+    const { email } = req.body;
 
-  if (!user) {
-    return res.send('Email not found');
-  }
+    const user = await usersRepo.getOneBy({ email });
 
-  const validPassword = await usersRepo.comparePasswords(
-    user.password,
-    password
-  );
-  if (!validPassword) {
-    return res.send('Invalid password');
-  }
+    req.session.userId = user.id;
 
-  req.session.userId = user.id;
-
-  res.send('You are signed in');
-});
+    res.send('You are signed in');
+  });
 
 module.exports = router;
